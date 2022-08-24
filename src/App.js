bb677ee591bb673c42bf3fdd90ad7184ccd503cb
jacheampong <jacheampong@gmail.com>
2022-08-23 22:25:42 -0400

    added aws amplify authentication

--- src/App.js
+++ src/App.js
@@ -1,87 +1,25 @@
-import React, {Component} from 'react'
-import Title from './components/Title'
-import Photos from './components/Photos'
-import './main.css'
-import {Posts} from './model/Posts'
-import AddPhoto from './components/AddPhoto'
-import {Route} from 'react-router-dom'
+import React from 'react';
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-export default class App extends Component {
-
-    // invoked immediately after a component is mounted
-    // update state with initial objects fetched
-    // Perfect place to fetch data. It gets called after render
-    componentDidMount() {
-        this.setState({
-            posts: Posts,
-            screen: 'photos', // photos, addPhoto
-        })
-        console.log('componentDidMount ...')
-    }
-
-    // called when the state of a component changes
-    // Perfect place to update UI or make network calls 
-    // based on previous state before update, and current state
-    componentDidUpdate(prevProps, prevState) {
-        console.log('componentDidUpdate ...')
-        console.log(prevProps)
-        console.log(prevState.posts)
-        console.log(this.state.posts)
-    }
-    
-    // called before component is mounted
-    constructor() {
-        super()
-        this.state = {
-            posts: []
-        }
-        console.log('constructor ...')
-    }
-
-    render() {
-        console.log('render ...')
-        console.log(this.state.posts)
-        return (
-            <div>
-                <Route path="/" exact render={() => (
-                    <div>
-                        <Title name={'Photowall'} />
-                        <Photos 
-                            photos={this.state.posts}
-                            removePhoto={this.removePhoto}
-                        />
-                    </div>
-                )} />
-
-                <Route path="/AddPhoto" exact render={({history}) => (
-                    <AddPhoto onAddPhoto={(addedPost) => {
-                        console.log(addedPost)
-                        this.addPhoto(addedPost)
-                        history.push('/')
-                    }} />
-                )} />
-
-            </div>
-        )
-    }
-
-    // method remove object passed from state object 
-    removePhoto = (photo) => {
-        console.log("In removePhoto ... ", photo.description)
-        // remove photo and update state
-        let newPost = this.state.posts.filter(post => post !== photo)
-        this.setState({
-            posts: newPost
-        })
-
-    }
-
-    addPhoto = (postSubmitted) => {
-        console.log('In addPhoto ...')
-        this.setState({
-            posts: this.state.posts.concat([postSubmitted])
-        })
-
-    }
-
+function App({ signOut }) {
+  return (
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
 }
+
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+Amplify.configure(config);
 
 ReactDOM.render(
   <BrowserRouter>
